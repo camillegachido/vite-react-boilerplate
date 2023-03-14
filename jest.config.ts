f31c62f4502ea3be8222e76f6ae8797c551d219a
Camille Gachido <camillegachido70@gmail.com>
2023-03-14 13:38:16 -0300

    add: initial configs

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+    testEnvironment: "jsdom",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    moduleNameMapper: {
+        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/mocks/fileMock.js',
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    },
+  }
